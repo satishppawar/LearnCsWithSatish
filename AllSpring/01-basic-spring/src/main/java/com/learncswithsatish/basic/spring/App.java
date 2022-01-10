a36d3d9618d301c11cc667010976c79b10b46d43
Your Name <you@example.com>
2022-01-11 01:07:45 +0530

    Initial changes

---
+++ AllSpring/01-basic-spring/src/main/java/com/learncswithsatish/basic/spring/App.java
@@ -0,0 +1,13 @@
+package com.learncswithsatish.basic.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
